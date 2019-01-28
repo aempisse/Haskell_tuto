bac0667037ea5f03edcd86688d64ca7d1fcec77c
aempisse <axelemp.ae@gmail.com>
2019-01-28 12:48:18 +0100

    composition

---
+++ composition.hs
@@ -0,0 +1,18 @@
+negateList :: (Num a) => [a] -> [a]
+negateList = map (\ x -> negate ( abs x))
+
+negateList' :: (Num a) => [a] -> [a]
+negateList' = map (negate . abs)
+
+oddSquareSum :: Integer  
+oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+oddSquareSum' :: Integer  
+oddSquareSum' = sum . takeWhile (<10000) . filter odd . map (^2) $ [1..]
+
+oddSquareSum'' :: Integer  
+oddSquareSum'' =   
+    let oddSquares = filter odd $ map (^2) [1..]  
+        belowLimit = takeWhile (<10000) oddSquares  
+    in  sum belowLimit
+    
